27407b60fdc801d37f8259f1245a929d6190d97a
chaeli <s.kcl5243@gmail.com>
2024-05-21 19:47:47 +0900

    FEAT: Item validation, service, model 추가

--- src/main/java/com/example/study/controller/book/ItemController.java
+++ src/main/java/com/example/study/controller/book/ItemController.java
@@ -1,7 +1,11 @@
 package com.example.study.controller.book;
 
 import com.example.study.entity.Item;
+import com.example.study.model.AddItemInput;
+import com.example.study.repository.BookRepository;
 import com.example.study.repository.ItemRepository;
+import com.example.study.service.ItemService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +17,13 @@ import java.util.List;
 
 @RestController
 public class ItemController {
-    @Autowired
+
     private ItemRepository itemRepository;
+    private ItemService itemService;
+    public ItemController(ItemRepository itemRepository, ItemService itemService) {
+        this.itemRepository = itemRepository;
+        this.itemService = itemService;
+    }
 
     @GetMapping("/items")
     public List<Item> getItems() {
@@ -22,9 +31,15 @@ public class ItemController {
         return items;
     }
 
+//    @PostMapping("/items")
+//    public ResponseEntity<Item> addItems(@RequestBody Item item) {
+//        Item savedItem = itemRepository.save(item);
+//        return ResponseEntity.ok(savedItem);
+//    }
+
     @PostMapping("/items")
-    public ResponseEntity<Item> addItems(@RequestBody Item item) {
-        Item savedItem = itemRepository.save(item);
-        return ResponseEntity.ok(savedItem);
+    public long addItems(@RequestBody @Valid AddItemInput input) {
+        long id = itemService.addItem(input);
+        return id;
     }
 }
