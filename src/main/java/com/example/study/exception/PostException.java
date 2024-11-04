9781a0a5af2e569d7abc61551023d0e5b798dab4
chaeli <s.kcl5243@gmail.com>
2024-11-05 04:55:01 +0900

    UPDATE: gitignore apply

--- src/main/java/com/example/study/exception/PostException.java
+++
@@ -1,7 +0,0 @@
-package com.example.study.exception;
-
-public class PostException extends RuntimeException {
-    public PostException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/study/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.study.repository;
-
-import com.example.study.model.AuthUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<AuthUser, Long> {
-    Optional<AuthUser> findByUsername(String username);
-}
