1e4a2e97db277ba4196fa4c35c33bdd42514fc41
chaeli <s.kcl5243@gmail.com>
2024-11-04 23:40:06 +0900

    TEST

--- src/main/java/com/example/study/model/AddItemInput.java
+++ src/main/java/com/example/study/model/AddItemInput.java
@@ -1,22 +1,22 @@
-package com.example.study.model;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Range;
-
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-public class AddItemInput {
-    @NotBlank(message = "상품 이름은 비워둘 수 없습니다.")
-    private String name;
-    @NotBlank(message = "상품 설명은 비워둘 수 없습니다.")
-    private String description;
-    @NotBlank(message = "시작 가격을 입력해야 합니다.")
-    @Range(min = 100, max = 100000,
-            message = "시작 가격은 최소 100원, 최대 10,000,000원 사이여야 합니다.")
-    private String startingPrice;
-}
+//package com.example.study.model;
+//
+//import jakarta.validation.constraints.NotBlank;
+//import jakarta.validation.constraints.Size;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import org.hibernate.validator.constraints.Range;
+//
+//@Getter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class AddItemInput {
+//    @NotBlank(message = "상품 이름은 비워둘 수 없습니다.")
+//    private String name;
+//    @NotBlank(message = "상품 설명은 비워둘 수 없습니다.")
+//    private String description;
+//    @NotBlank(message = "시작 가격을 입력해야 합니다.")
+//    @Range(min = 100, max = 100000,
+//            message = "시작 가격은 최소 100원, 최대 10,000,000원 사이여야 합니다.")
+//    private String startingPrice;
+//}
--- src/main/java/com/example/study/model/ErrorResponse.java
+++ src/main/java/com/example/study/model/ErrorResponse.java
@@ -1,13 +1,13 @@
-package com.example.study.model;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-import java.util.List;
-
-@Getter
-@AllArgsConstructor
-public class ErrorResponse {
-    private int errorCode;
-    private List<String> errorMessages;
-}
+//package com.example.study.model;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//
+//import java.util.List;
+//
+//@Getter
+//@AllArgsConstructor
+//public class ErrorResponse {
+//    private int errorCode;
+//    private List<String> errorMessages;
+//}
